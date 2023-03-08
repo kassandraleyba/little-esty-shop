4a1f1d974fd3498187dee1d508d6f1703f1ee7b8
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-03-08 11:29:44 -0700

    Add range for form

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,6 +2,6 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
   has_many :items, through: :merchant
 
-  validates_presence_of :percentage_discount
-  validates_numericality_of :quantity_threshold
+  validates_presence_of :percentage_discount, numericality: { greater_than: 0, less_than_or_equal_to: 1 }
+  validates_numericality_of :quantity_threshold, { only_integer: true, greater_than_or_equal_to: 1 }
 end
